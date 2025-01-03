56eca30c6a44c67694bea8e2e5c3b3372f08abe4
Vedant Mahajan <vedantmahajan271@gmail.com>
2025-01-03 17:41:51 +0530

    Project Start Configuration

--- dist/index.js
+++ dist/index.js
@@ -14,26 +14,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const commander_1 = require("commander");
 const config_1 = require("./config");
-const node_cluster_1 = __importDefault(require("node:cluster"));
+const server_1 = require("./server");
 const node_os_1 = __importDefault(require("node:os"));
-const node_http_1 = __importDefault(require("node:http"));
-function createServer(config) {
-    return __awaiter(this, void 0, void 0, function* () {
-        const { workerCount } = config;
-        if (node_cluster_1.default.isPrimary) {
-            console.log('Master Process is Up');
-            for (let i = 0; i < workerCount; i++) {
-                node_cluster_1.default.fork({ config: JSON.stringify(config.config) });
-                console.log(`Master Process: Worker Node spinned up ${i + 1}`);
-            }
-            const server = node_http_1.default.createServer(function (req, res) {
-            });
-        }
-        else {
-            console.log('Worker Node', process.env.config);
-        }
-    });
-}
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
         var _a;
@@ -42,7 +24,7 @@ function main() {
         const options = commander_1.program.opts();
         if (options && 'config' in options) {
             const validatedConfig = yield (0, config_1.validateConfig)(yield (0, config_1.parseYamlConfig)(options.config));
-            yield createServer({ port: validatedConfig.server.listen, workerCount: (_a = validatedConfig.server.workers) !== null && _a !== void 0 ? _a : node_os_1.default.cpus().length, config: validatedConfig });
+            yield (0, server_1.createServer)({ port: validatedConfig.server.listen, workerCount: (_a = validatedConfig.server.workers) !== null && _a !== void 0 ? _a : node_os_1.default.cpus().length, config: validatedConfig });
         }
     });
 }
--- src/index.ts
+++ src/index.ts
@@ -1,35 +1,7 @@
 import { program } from "commander";
 import { validateConfig, parseYamlConfig } from "./config";
-import  cluster  from 'node:cluster'
+import { createServer } from "./server";
 import os from 'node:os'
-import http from 'node:http'
-import { ConfigSchemaType } from "./config-schema";
-
-interface serverConfig {
-    port: number
-    workerCount: number
-    config: ConfigSchemaType
-}
-
-async function createServer(config: serverConfig) {
-    const { workerCount } = config;
-
-    if(cluster.isPrimary){
-        console.log('Master Process is Up');
-
-        for(let i = 0;  i < workerCount; i++){
-            cluster.fork({config: JSON.stringify(config.config)});
-            console.log(`Master Process: Worker Node spinned up ${i + 1}`)
-        }
-
-        const server = http.createServer(function(req, res){
-
-        })
-
-    } else {
-        console.log('Worker Node', process.env.config)
-    }    
-}
 
 async function main() {
     program.option('--config <path>');
